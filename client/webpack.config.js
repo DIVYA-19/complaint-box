fb8b40d6ee205c042ae6be59972a0c25e49f12af
DIVYA-19 <divyareddy.y019@gmail.com>
2022-03-09 21:37:48 +0530

    env variables

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,23 +6,35 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const port = process.env.PORT || 3000;
 
+// const getClientEnv = () => {
+//   const currentPath = path.join(__dirname);
+//   const basePath = currentPath + "/.env";
+//   const envPath = basePath  + "." + env.ENVIRONMENT;
+//   const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+//   const fileEnv = dotenv.config({ path: finalPath }).parsed;
+//   const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+//     prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+//     return prev;
+//   }, {});
+
+//   return {
+//     envKeys
+//   };
+// };
+
 const getClientEnv = () => {
-  const currentPath = path.join(__dirname);
-  const basePath = currentPath + "/.env";
-  const envPath = basePath // + "." + env.ENVIRONMENT;
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-  const fileEnv = dotenv.config({ path: finalPath }).parsed;
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+  env = dotenv.config().parsed;
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
   }, {});
+
+  console.log(envKeys)
 
   return {
     envKeys
   };
 };
-
-
 
 
 module.exports = {
@@ -40,7 +52,7 @@ module.exports = {
     new webpack.ProvidePlugin({
       "React": "react",
    }),
-  //  new webpack.DefinePlugin(getClientEnv()),
+   new webpack.DefinePlugin(getClientEnv()),
    new webpack.EnvironmentPlugin( { ...process.env } )
   ],
   module: {
