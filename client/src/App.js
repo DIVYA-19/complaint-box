ef64ccab67d536fc14b5d5ecf63e0ab40bce472c
DIVYA-19 <divyareddy.y019@gmail.com>
2022-03-17 23:15:28 +0530

    add requireAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,41 @@
 import React from "react";
+import { BrowserRouter as Router, Navigate } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 import "./App.css";
 import Complaints from "./components/Complaints/Complaints";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Routes, Route, Link } from "react-router-dom";
 import Signin from "./components/Signin/Signin";
 import Signup from "./components/Signup/Signup";
+import RequireAuth from "./requireAuth";
+
+function requireAuth() {
+  const token = localStorage.getItem("@token");
+  console.log("requie auth ", token);
+  if (!token) <Navigate path="/signin" />;
+}
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/signin" element={<Signin />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/complaints" element={<Complaints />} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/signin" element={<Signin />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route element={<RequireAuth />}>
+              <Route
+                exact
+                path="/complaints"
+                element={<Complaints />}
+                onEnter={requireAuth}
+              />
+            </Route>
+            {/* <Route path="*" element={<NotFound />} /> */}
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ client/src/requireAuth.js
@@ -0,0 +1,26 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { connect } from 'react-redux'
+
+function RequireAuth({ isLoggedIn }) {
+    // let auth = useAuth();
+    let location = useLocation();
+
+    if (!isLoggedIn) {
+      // Redirect them to the /login page, but save the current location they were
+      // trying to go to when they were redirected. This allows us to send them
+      // along to that page after they login, which is a nicer user experience
+      // than dropping them off on the home page.
+      return <Navigate to="/signin" state={{ from: location }} />;
+    }
+  
+    return <Outlet />;
+  }
+
+  function mapStateToProps(state, props) {
+    console.log("in require auth", state)
+    return {
+      isLoggedIn: state.data.isLoggedIn
+    }
+  }
+
+  export default connect(mapStateToProps)(RequireAuth);
