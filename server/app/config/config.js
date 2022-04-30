24d88bb1de07b76183fbdb23be6e89523d4eadc1
DIVYA-19 <divyareddy.y019@gmail.com>
2022-04-30 16:26:32 +0530

    add refresh token expiration time constant

--- server/app/config/config.js
+++ server/app/config/config.js
@@ -5,5 +5,6 @@ module.exports = {
     jwtSecret: process.env.JWT_SECRET,
     refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
     expireToken: process.env.EXPIRES,
+    jwtRefreshExpiration: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
     states: ["open", "closed", "pending", "in progess"]
 }
